52e31ba6333d605ee91ab1b5feb4cf3c1ebdf92f
sebasSFU <smora@sfu.ca>
2021-01-19 22:53:01 -0800

    minpaper and name.py

---
+++ name.py
@@ -0,0 +1,16 @@
+firstname ="Sebastian"  
+lastname ="Mora"
+preferredname ="Sebastian"
+pronouns ="he/him" 
+favnumb =1066
+
+print( firstname + " " + lastname )
+print( preferredname + " " + pronouns )
+print( "My favourite number is  %d" %(favnumb))
+print(favnumb*favnumb) #simple multiplication 
+print(favnumb + 10)  # simple addition
+remainder = favnumb%7
+print(favnumb%7)  # remainder of the favnumb mod 7 
+quotient = favnumb/7
+print(favnumb/7 ) #integer division 
+print( "%d * 7 + %d = %d" %(quotient, remainder, favnumb)) 
